8ced119033cce00a2a5c09642f417270489467aa
Rishabh <rishabhvrm43@gmail.com>
2024-07-04 12:51:24 -0700

    add Questions for kunal interview July 4

---
+++ Mock Interview/kunal-interview/04 July 2024/Q2.py
@@ -0,0 +1,20 @@
+'''
+LC 844
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. 
+'#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+'''
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        pass
+
