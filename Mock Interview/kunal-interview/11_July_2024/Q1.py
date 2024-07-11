2c20bd067c57d91627443fe5998bb96ff2d85ab4
Kunal Chand <kunalchand234@gmail.com>
2024-07-11 18:45:29 -0400

    mock solutions

--- Mock Interview/kunal-interview/11_July_2024/Q1.py
+++ Mock Interview/kunal-interview/11_July_2024/Q1.py
@@ -1,4 +1,4 @@
-'''
+"""
 LC. 643
 
 You are given an integer array nums consisting of n elements, and an integer k.
@@ -13,8 +13,25 @@ Input: nums = [1,12,-5,-6,50,3], k = 4
 Output: 12.75000
 Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
 
-'''
+ [1,12,-5,-6,50,3] k = 4 O(k) O(1)
+        L       R
+"""
+
 from typing import List
+
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        pass
+        window_sum = sum(nums[:k])
+        max_average = window_sum / k
+
+        left = 0
+        for right in range(k, len(nums)):
+            window_sum -= nums[left]
+            left += 1
+            window_sum += nums[right]
+
+            window_average = window_sum / k
+            max_average = max(max_average, window_average)
+
+        return max_average
--- Mock Interview/kunal-interview/11_July_2024/Q2.py
+++ Mock Interview/kunal-interview/11_July_2024/Q2.py
@@ -1,4 +1,4 @@
-'''
+"""
 LC. 442
 
 Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
@@ -9,12 +9,30 @@ You must write an algorithm that runs in O(n) time and uses only constant extra 
 
 Example 1:
 
-Input: nums = [4,3,2,7,8,2,3,1]
+Input: nums = [4,4,2,7,8,2,3,1]
 Output: [2,3]
 
-'''
+"""
 
 from typing import List
+
+"""
+[4,-4,2,-7,8,2,-3,1]
+           i
+
+
+"""
+
+
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        pass
+        ans = []
+
+        for index in range(len(nums)):
+            value = abs(nums[index])
+            if nums[value - 1] < 0:
+                ans.append(value)
+            else:
+                nums[value - 1] *= -1
+
+        return ans
